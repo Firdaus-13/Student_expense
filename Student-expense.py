08c80326dce94b69e8e22d62ef11767d86d16a07
wanhaziq98 <wanhaziqwankamarul@gmail.com>
2020-08-24 01:19:06 +0800

    Update Student-expense.py

--- Student-expense.py
+++ Student-expense.py
@@ -2,3 +2,10 @@ from tkinter import *
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.messagebox
+
+
+class Application(tk.Frame):
+
+
+app = Application(tk.Tk())
+app.root.mainloop()
