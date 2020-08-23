21637c5bef8978eacf0a04b6c507bf0a1158d36e
adikordi98 <69725622+adikordi98@users.noreply.github.com>
2020-08-24 02:33:41 +0800

    add tab
    amount,description date

--- Student-expense.py
+++ Student-expense.py
@@ -17,6 +17,22 @@ class Application(tk.Frame):
         self.root.grid_columnconfigure(0, weight=1)
         self.root.config(background="white")
         
+         # Define the different GUI widgets
+        self.Amount_label = tk.Label(self.root, text="Amount (RM) :")
+        self.Amount_entry = tk.Entry(self.root)
+        self.Amount_label.grid(row=0, column= 0 ,sticky = tk.W)
+        self.Amount_entry.grid(row=0, column=1,sticky = tk.W)
+ 
+        self.Description_label = tk.Label(self.root, text="Description     :")
+        self.Description_entry = tk.Entry(self.root)
+        self.Description_label.grid(row=1, column=0 ,sticky = tk.W)
+        self.Description_entry.grid(row=1, column=1,sticky = tk.W)
+        
+        self.Date_label = tk.Label(self.root, text="Date                 :")
+        self.Date_entry = tk.Entry(self.root)
+        self.Date_label.grid(row=2, column= 0, sticky = tk.W)
+        self.Date_entry.grid(row=2, column=1,sticky = tk.W)
+        
         
 
 app = Application(tk.Tk())
